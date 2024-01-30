7b6c6276ad01aad94cf8a067f026e72844e923fd
RosieOh <20172207@gm.hannam.ac.kr>
2024-01-31 01:35:49 +0900

    🏧build : 계죄시스템 초기 설정🏧

---
+++ src/main/java/com/paymind/PaymindApplication.java
@@ -0,0 +1,13 @@
+package com.paymind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paymind/PaymindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
